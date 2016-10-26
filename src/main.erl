7396b2502e42e636fe3fbafbc5b5e5ecf361b0e6
Roberto Nogueira <enogrob@gmail.com>
2016-10-26 06:45:25 -0200

    add designing-for-scalability-with-erlang

---
+++ src/main.erl
@@ -0,0 +1,15 @@
+%%%-------------------------------------------------------------------
+%%% @author enogrob
+%%% @copyright (C) 2016, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 25. Oct 2016 23:51
+%%%-------------------------------------------------------------------
+-module(main).
+-author("enogrob").
+
+%% API
+-export([print_helloworld/0]).
+print_helloworld() ->
+  io:format("Hello World!").
